5981ed6d269103ff20994c5be4e97876018e76fb
in-sang-cho <perrot_1@naver.com>
2022-06-17 12:53:31 +0900

    map v0.3

--- Map/Map/main.cpp
+++ Map/Map/main.cpp
@@ -1,43 +1,104 @@
-// map v0.2
+// map v0.3
 
 #include <iostream>
+#include <string>
 #include <map>
+#include <list>
+#include <vector>
 
 using namespace std;
 
-map<int, int> Numbers;
+struct Vector3
+{
+	float x, y, z;
 
-void AddData(int _key, int _value);
+	Vector3() : x(0.0f), y(0.0f), z(0.0f) {};
+
+	Vector3(const float& _x, const float& _y) : x(_x), y(_y), z(0.0f) {};
+
+	Vector3(const float& _x, const float& _y, const float& _z) : x(_x), y(_y), z(_z) {};
+};
+
+struct Transform
+{
+	Vector3 Position;
+	Vector3 Rotation;
+	Vector3 Scale;
+};
+
+struct Object
+{
+	Transform Info;
+
+	Object() {};
+
+	Object(const Transform& _info) : Info(_info) {};
+};
+
+struct Player : public Object
+{
+	Player() {};
+
+	Player(const Transform& _info) : Object(_info) {};
+};
+
+map<string, list<Object*>> Objects;
+
+void Initalize();
+void AddData(string _key, Object* _object);
 
 int main(void)
 {
-	Numbers[0] = 0;
-	Numbers[1] = 10;
-	//Numbers[2] = 20;
-	AddData(2, 200);
-	Numbers[3] = 30;
+	Transform Info;
 
-	// ** 최초 한 번은 이렇게 넣어도 상관없다 (Value가 없는 것이 확실하기 때문)
-	Numbers[1] = 100;
+	Info.Position.x = 10;
+	Info.Position.y = 20;
+	Info.Position.z = 40;
 
-	Numbers.insert(make_pair(4, 40));
+	AddData("Player", new Player(Info));
 
-	// ** 출력
-	for (map<int, int>::iterator iter = Numbers.begin(); iter != Numbers.end(); iter++)
-		cout << iter->second << endl;
+	//cout << Objects["Player"] = Info.Position.x << endl;
+	//cout << Objects["Player"] = Info.Position.y << endl;
+	//cout << Objects["Player"] = Info.Position.z << endl;
 
 	return 0;
 }
 
 // hash table 이라는 구조를 만들어서 시작할 때 데이터를 다 집어넣는다
 
-void AddData(int _key, int _value)
+void Initalize()
 {
-	// ** 기존 데이터를 지키면서 Value를 추가하고 싶을 때
-	map<int, int>::iterator iter = Numbers.find(_key);
+	Transform Info;
 
-	if (iter == Numbers.end())
-		Numbers.insert(make_pair(_key, _value));
+	Info.Position.x = 10;
+	Info.Position.y = 20;
+	Info.Position.z = 40;
+
+	//Objects["Player"] = new Player(Info);
+}
+
+void AddData(string _key, Object* _object)
+{
+	map<string, list<Object*>> ::iterator iter = Objects.find(_key);
+
+	if (iter == Objects.end())
+	{
+		list<Object*> Temp;
+		Temp.push_back(_object);
+		Objects.insert(make_pair(_key, Temp));
+	}
 	else
-		iter->second = _value;
+		iter->second.push_back(_object);
+
+
+	/*
+	// ** 기존 데이터를 지키면서 Value를 추가하고 싶을 때
+	map<int, Object*>::iterator iter = Objects.find(_key);
+
+	if (iter == Objects.end())
+		Objects.insert(make_pair(_key, _object));
+		Objects.insert(make_pair(_key, _object));
+	else
+		iter->second = _object;
+	*/
 }
